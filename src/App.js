c2536c416b3c0ada45b824da488aedbd1e2845e2
Paul Jimenez <paul@rocketlabs.mx>
2020-05-04 17:49:25 -0700

    Home page broken down into components and finished styling for menu item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import  HomePage  from './home-page.component'
+import  HomePage  from './pages/homepage/home-page.component'
 
 function App() {
   return (
