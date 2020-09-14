0f744a486e535cd7a3671f37574e530491eb0b61
Paul Jimenez <pjf2180@me.com>
2020-09-14 12:45:11 -0700

    Fixed timestamp to str

--- client/src/components/order-product-header/order-product-header.component.jsx
+++ client/src/components/order-product-header/order-product-header.component.jsx
@@ -4,7 +4,7 @@ import moment from 'moment'
 export default function OrderProductHeader({ timestamp, total }) {
     console.log(timestamp)
     const date = new Date(timestamp);
-    const dateStr = moment(date).format('MMMM Do YYYY');
+    const dateStr = moment.unix(timestamp).format('MMMM Do YYYY');
     return (
         <div className="order__section order__header">
             <div className="order__date-container">
